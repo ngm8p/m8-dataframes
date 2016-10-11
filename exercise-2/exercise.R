943332a91767fe3011116e67920696e70fd431a2
Meghan Ng <ngmeghan8@gmail.com>
2016-10-11 14:23:34 -0700

    Completed exercise-2

--- exercise-2/exercise.R
+++ exercise-2/exercise.R
@@ -2,21 +2,31 @@
 
 # Create a vector of the number of points the Seahawks have scored in each game this season (google "Seahawks")
 
+points.won <- c(12, 3, 37, 27)
 
 # Create a vector of the number of points the Seahwaks have allowed to be scored against them in each game this season
 
+points.lost <- c(10, 9, 18, 17)
 
 # Combine your two vectors into a dataframe
 
+seahawks.data <- data.frame(points.won, points.lost)
 
 # Create a new column "diff" that is the difference in points
 
+seahawks.data$diff <- abs(points.won - points.lost)
 
 # Create a new column "won" which is TRUE if the Seahawks wom
+
+seahawks.data$won <- points.won > points.lost
 
 
 # Create a vector of the opponents
 
+opponents <- c("Dolphins", "Rams", "49ers", "Jets")
+
 
 # Assign your dataframe rownames of their opponents
+
+seahawks.data$opponents <- opponents
 
